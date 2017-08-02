b4ec7d58ce1b9b7aa7dffdd328ab6fec724f921a
hellocecile <happyoupi@gmail.com>
2017-08-02 15:54:23 +0200

    Ajustements CSS

--- beads_fichiers/js/main.js
+++ beads_fichiers/js/main.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
 
-	//var colorClass = '';
+	var colorClass = '';
 
-	// $('.select-color').click(function() {
-	// 	var selectedColor = $(this).attr('class');
-	//
-	// 	switch (selectedColor) {
-	// 		case "select-color cyan not-selected":
-	// 			colorClass = 'cyan';
-	// 			break;
-	// 		case "select-color yellow not-selected":
-	// 			colorClass = 'yellow';
-	// 			break;
-	// 		case "select-color magenta not-selected":
-	// 			colorClass = 'magenta';
-	// 			break;
-	// 		case "select-color purple not-selected":
-	// 			colorClass = 'purple';
-	// 			break;
-	// 		case "select-color pink not-selected":
-	// 			colorClass = 'pink';
-	// 			break;
-	// 		case "select-color green not-selected":
-	// 			colorClass = 'green';
-	// 			break;
-	// 	}
-	//
-	// 	$(this).removeClass('not-selected');
-	// 	$(this).siblings().addClass('not-selected');
-	// });
+	$('.select-color').click(function() {
+		var selectedColor = $(this).attr('class');
 
-	// // au clique sur une div
-	// $('.box').on("click", function() {
-	// 	$(this).toggleClass(colorClass);
-	// });
+		switch (selectedColor) {
+			case "select-color cyan not-selected":
+				colorClass = 'cyan';
+				break;
+			case "select-color yellow not-selected":
+				colorClass = 'yellow';
+				break;
+			case "select-color magenta not-selected":
+				colorClass = 'magenta';
+				break;
+			case "select-color purple not-selected":
+				colorClass = 'purple';
+				break;
+			case "select-color pink not-selected":
+				colorClass = 'pink';
+				break;
+			case "select-color green not-selected":
+				colorClass = 'green';
+				break;
+		}
+
+		$(this).removeClass('not-selected');
+		$(this).siblings().addClass('not-selected');
+	});
+
+	// au clique sur une div
+	$('.box').on("click", function() {
+		$(this).toggleClass(colorClass);
+	});
 
 
 });
