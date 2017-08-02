43b789d676b052e0f57719407ab9bd09c23ae93a
hellocecile <happyoupi@gmail.com>
2017-08-03 00:12:43 +0200

    Début de loop pour le colorpicker

--- beads_fichiers/js/beads.js
+++ beads_fichiers/js/beads.js
@@ -2,35 +2,54 @@
 
 $(document).ready(function(){
 
-    var color = '';
+    // tableau de couleurs
+    var colors = [
+        "cyan",
+        "blue",
+        "yellow",
+        "orange",
+        "red",
+        "magenta",
+        "pink",
+        "green",
+    ];
 
-	$('.select-color').click(function() {
-		var selectedColor = $(this).attr('class');
+    var pickColor = $(".pick-color");
 
-		switch (selectedColor) {
-			case "select-color cyan not-selected":
-				color = 'cyan';
-				break;
-			case "select-color yellow not-selected":
-				color = 'yellow';
-				break;
-			case "select-color magenta not-selected":
-				color = 'magenta';
-				break;
-			case "select-color purple not-selected":
-				color = 'purple';
-				break;
-			case "select-color pink not-selected":
-				color = 'pink';
-				break;
-			case "select-color green not-selected":
-				color = 'green';
-				break;
-		}
+    $.each(colors, function(key, color){
+        pickColor.append('<div class="pick"></div>');
+        $(".pick").css("background-color", color);
+    });
 
-		$(this).removeClass('not-selected');
-		$(this).siblings().addClass('not-selected');
-	});
+    // var color = '';
+    //
+	// $('.select-color').click(function() {
+	// 	var selectedColor = $(this).attr('class');
+    //
+	// 	switch (selectedColor) {
+	// 		case "select-color cyan not-selected":
+	// 			color = 'cyan';
+	// 			break;
+	// 		case "select-color yellow not-selected":
+	// 			color = 'yellow';
+	// 			break;
+	// 		case "select-color magenta not-selected":
+	// 			color = 'magenta';
+	// 			break;
+	// 		case "select-color purple not-selected":
+	// 			color = 'purple';
+	// 			break;
+	// 		case "select-color pink not-selected":
+	// 			color = 'pink';
+	// 			break;
+	// 		case "select-color green not-selected":
+	// 			color = 'green';
+	// 			break;
+	// 	}
+    //
+	// 	$(this).removeClass('not-selected');
+	// 	$(this).siblings().addClass('not-selected');
+	// });
 
 
     var grid = $('div.grid');
