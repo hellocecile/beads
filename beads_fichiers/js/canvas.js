aabd0d94d660eb4bef7c80c543e8d720718286ba
hellocecile <happyoupi@gmail.com>
2017-08-02 16:50:07 +0200

    Boucle refaite

--- beads_fichiers/js/canvas.js
+++ beads_fichiers/js/canvas.js
@@ -2,17 +2,49 @@
 
 $(document).ready(function(){
 
-    var rows = [];
-    for(var i = 1; i <= 15; i++){
-        rows.push('<div class="row"></div>');
+    var colorClass = '';
+
+	$('.select-color').click(function() {
+		var selectedColor = $(this).attr('class');
+
+		switch (selectedColor) {
+			case "select-color cyan not-selected":
+				colorClass = 'cyan';
+				break;
+			case "select-color yellow not-selected":
+				colorClass = 'yellow';
+				break;
+			case "select-color magenta not-selected":
+				colorClass = 'magenta';
+				break;
+			case "select-color purple not-selected":
+				colorClass = 'purple';
+				break;
+			case "select-color pink not-selected":
+				colorClass = 'pink';
+				break;
+			case "select-color green not-selected":
+				colorClass = 'green';
+				break;
+		}
+
+		$(this).removeClass('not-selected');
+		$(this).siblings().addClass('not-selected');
+	});
+
+
+    var grid = $('div.grid');
+    for(var i=1; i<=15; i++){
+        grid.append('<div class="row"></div>');
     }
 
-    var boxes = [];
-    for(var i = 1; i <= 15; i++){
-        boxes.push('<div class="box"></div>');
+    var row = $('div.row');
+    for(var j=1; j<=15; j++){
+        row.append('<div class="box"></div>');
     }
 
-    $('div.grid').append(rows.join(""));
-    $('div.row').append(boxes.join(""));
+    $(".box").on("click", function() {
+        $(this).toggleClass(colorClass);
+    });
 
 });
