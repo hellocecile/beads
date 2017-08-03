ed6d73c40b14b236d23d0cf50d3192c48cf67a92
hellocecile <happyoupi@gmail.com>
2017-08-03 12:44:14 +0200

    Base version 1.x

--- beads_fichiers/js/beads.js
+++ beads_fichiers/js/beads.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-    // tableau de couleurs
+    // Tableau de couleurs
     var colors = [
         "cyan",
         "blue",
@@ -14,44 +14,7 @@ $(document).ready(function(){
         "green",
     ];
 
-    var pickColor = $(".pick-color");
-
-    $.each(colors, function(key, color){
-        pickColor.append('<div class="pick"></div>');
-        $(".pick").css("background-color", color);
-    });
-
-    // var color = '';
-    //
-	// $('.select-color').click(function() {
-	// 	var selectedColor = $(this).attr('class');
-    //
-	// 	switch (selectedColor) {
-	// 		case "select-color cyan not-selected":
-	// 			color = 'cyan';
-	// 			break;
-	// 		case "select-color yellow not-selected":
-	// 			color = 'yellow';
-	// 			break;
-	// 		case "select-color magenta not-selected":
-	// 			color = 'magenta';
-	// 			break;
-	// 		case "select-color purple not-selected":
-	// 			color = 'purple';
-	// 			break;
-	// 		case "select-color pink not-selected":
-	// 			color = 'pink';
-	// 			break;
-	// 		case "select-color green not-selected":
-	// 			color = 'green';
-	// 			break;
-	// 	}
-    //
-	// 	$(this).removeClass('not-selected');
-	// 	$(this).siblings().addClass('not-selected');
-	// });
-
-
+    // Création de la grille
     var grid = $('div.grid');
     for(var i=1; i<=15; i++){
         grid.append('<div class="row"></div>');
@@ -62,8 +25,20 @@ $(document).ready(function(){
         row.append('<div class="box"></div>');
     }
 
-    $(".box").on("click", function() {
-        $(this).css("background-color", color);
+    // Color picker
+    var pickColor = $(".pick-color");
+
+    $.each(colors, function(key, color){
+        pickColor.append('<div class="pick" style="background-color:'+ color +'" data-color="'+ color +'"></div>');
+
+        $(".pick").click(function(){
+            color = $(this).attr("data-color");
+        });
+
+        $(".box").on("click", function() {
+            $(this).css("background-color", color);
+        });
     });
+
 
 });
